e38a1253a3ed68c0e5a01764f3f5bcdd0e354454
Trash Panda <d.george.schlosser@gmail.com>
2019-04-11 18:30:48 -0400

    added some variable and psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+/*PSUEDOCODE:
+when start button pressed > change DOM to display questions & answers and start timer;
+user selects answers
+quiz ends when user presses submit || timer === 0;
+when game ends > compare user answer w/ correct answer
+    if answer === correct answer > correct++
+    else incorrect++
+*/
+
+// VARIABLES
+// ===================================
+var gameStart = false;
+var gameEnd = false;
+var timer = 30;
+var questions = [];
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+
+
+
+// FUNCTIONS
+// ===================================
+
+
+
+// MAIN FUNCTIONS
+// ===================================
+// when start is pressed gameStart = true;
+    // if gameStart = true display timer and questions
+
+

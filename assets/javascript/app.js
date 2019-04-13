3a052bbee61181d0ea833579ca01484c5663f88e
Trash Panda <d.george.schlosser@gmail.com>
2019-04-12 22:29:59 -0400

    Questions and answers saved into array of objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,38 @@ var gameStart = false;
 var gameEnd = false;
 var timer = 30;
 var timerId;    //holds interval ID when executed
-var questions = [];
+var quiz = [
+    {
+        question: "What creature's skeleton does C-3PO walk past on Tatoine?",
+        choices: ["Wookie", "Sarlacc", "Krayt Dragon", "Bantha"],
+        correct: 3
+    },
+
+    {
+        question: "Few people believed in Star Wars. Which Hollywood director did?",
+        choices: ["George Lucas", "Brian De Palma", "Stanley Kubrick", "Steven Spielberg"],
+        correct: 4
+    },
+
+    {
+        question: "In regards to the script, which actor said, 'You can type this shit, but you can't say it.'",
+        choices: ["Sir Alec Guinnes", "Peter Cushing", "Carrie Fisher","Harrison Ford"],
+        correct: 3
+    },
+
+    {
+        question: "Which character says, 'May the Force be with you.'",
+        choices: ["Han Solo", "Obi-wan Kenobi", "Princess Leia", "Luke Skywalker"],
+        correct: 1
+    },
+
+    {
+        question: "What is the basis of the Jawas' language?",
+        choices: ["Spanish", "Zulu", "Portuguese", "Afrikanns"],
+        correct: 2
+    },
+
+];
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
